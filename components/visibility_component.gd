383c5c8198500b1aa98d4275ee077a93b9ec06bb
Dallas <dgreene1641@gmail.com>
2026-01-31 14:52:23 -0500

    add new base class entity for player and enemies

--- components/visibility_component.gd
+++ components/visibility_component.gd
@@ -4,16 +4,14 @@ class_name VisibilityComponent extends Node2D
 @export var is_player: bool = false
 
 
-var parent_entity: Node2D = null
+var parent_entity: Entity = null
 var visibility_id: int = -1
 
 
 func _ready() -> void:
-	add_to_group("visible")
-	assign_parent_entity.call_deferred()
 	visibility_id = IdGenerator.generate_visibility_id()
-
-
-func assign_parent_entity() -> void:
-	parent_entity = get_parent()
 	Visibility.register_visibility_component(self)
+
+
+func assign_parent_entity(new_parent_entity: Entity) -> void:
+	parent_entity = new_parent_entity
--- globals/id_generator.gd
+++ globals/id_generator.gd
@@ -2,6 +2,12 @@ extends Node
 
 
 var next_visibility_id: int = 0
+var next_entity_id: int = 0
+
+
+func generate_entity_id() -> int:
+	next_entity_id += 1
+	return next_entity_id - 1
 
 
 func generate_visibility_id() -> int:
