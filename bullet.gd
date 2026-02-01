827199ae954b0e588f8d163984e3e55ebc26d6fb
Adam Freedman <dama33@gmail.com>
2026-02-01 12:59:47 -0500

    added new navigation regions and also added another queue free functionality to bullets

--- bullet.gd
+++ bullet.gd
@@ -10,11 +10,15 @@ func _ready() -> void:
 	$HitArea2D.hit_somone.connect(_on_hit_someone)
 	look_at(target_position)
 	direction_fired = (target_position - global_position).normalized()
-
+	
 
 func _physics_process(delta: float) -> void:
 	position += direction_fired * delta * 2000
 
 
 func _on_hit_someone() -> void:
+	queue_free()
+
+
+func _on_timer_timeout() -> void:
 	queue_free()
