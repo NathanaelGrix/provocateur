4b91e045c79af739bcbf0837e7f9e2c7bfb4a908
spyoder93 <157861097+spyoder93@users.noreply.github.com>
2026-02-01 14:09:10 -0500

    cowboy and alien swapping

--- enemy.gd
+++ enemy.gd
@@ -5,13 +5,15 @@ const SPEED: float = 50000
 var next_position: Vector2
 var bullet = preload("res://bullet.tscn")
 
+
 func _ready() -> void:
 	super()
 	await get_tree().physics_frame
-	%NavigationAgent2D.target_position = NavigationServer2D.region_get_random_point(navigation_region.get_rid(),%NavigationAgent2D.navigation_layers, false)
+	_on_navigation_agent_2d_navigation_finished.call_deferred()
 	health_component.health_depleted.connect(_kill_enemy)
 	$AttackCooldownTimer.timeout.connect(_on_attack_off_cooldown)
-	
+
+
 func _physics_process(delta: float) -> void:
 	super(delta)
 	if !%NavigationAgent2D.is_navigation_finished():
@@ -19,10 +21,16 @@ func _physics_process(delta: float) -> void:
 		velocity = global_position.direction_to(next_position).normalized() * SPEED * delta
 	else:
 		velocity = Vector2.ZERO
+		
+	set_animation()
 	move_and_slide()
 
 
 func create_bullet():
+	if enemy_details and enemy_details.gunshot_sound:
+		$AudioStreamPlayer2D_Gun.stream = enemy_details.gunshot_sound
+		$AudioStreamPlayer2D_Gun.play()
+	
 	var created_bullet = bullet.instantiate()
 	created_bullet.parentEntity = self
 	created_bullet.position = position
@@ -47,12 +55,14 @@ func _on_timer_timeout() -> void:
 	
 #Not sure how to set it up to play proper animations - Sam
 func set_animation():
-	if velocity != Vector2.ZERO:
-		$AnimatedSprite2D.animation("walk")
-		$AnimatedSprite2D.play()
+	if enemy_details == null:
+		return
+
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play(enemy_details.walk_animation)
 	else:
-		$AnimatedSprite2D.animation("idle")
-		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.play(enemy_details.idle_animation)
+		
 
 
 # kill the enemy if it has lost all of it's health
